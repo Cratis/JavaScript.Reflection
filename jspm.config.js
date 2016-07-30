f7c76837bde4db515a06933634030c4491ba778b
Einar Ingebrigtsen <einar@dolittle.com>
2016-07-30 12:56:12 -0700

    Changing from src to Source

--- jspm.config.js
+++ jspm.config.js
@@ -1,6 +1,6 @@
 SystemJS.config({
     paths: {
-        "cratis-javascript-reflection/": "src/"
+        "cratis-javascript-reflection/": "Source/"
     },
     browserConfig: {
         "paths": {
